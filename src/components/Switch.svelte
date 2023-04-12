ef044ea8132792ec51baa17be564d017dfa3686f
daniel.a <daniel.arroyo@geopagos.com>
2023-04-12 19:54:00 -0300

    first

---
+++ src/components/Switch.svelte
@@ -0,0 +1,34 @@
+<script context="module" lang="ts">
+	import { writable } from 'svelte/store';
+	export const switchedOn = writable<(string | number)[]>([]);
+</script>
+
+<script lang="ts">
+
+	export let identifier:string | number;
+
+	const handleSwitch = () => {
+		switchedOn.set([identifier]);
+	};
+
+	/* caracter $ delante de switchedOn me ahorra de hacer switchedOn.subscribe (autosubscribe)*/
+	$: isSwitchOn = $switchedOn[0] === identifier;
+</script>
+
+<button on:click={handleSwitch}>
+	{isSwitchOn ? '💡' : '⛔'}
+</button>
+
+<style>
+	button {
+		font-size: 3rem;
+		color: white;
+		cursor: pointer;
+		background: none;
+		border: 2px solid black;
+		padding: 15px;
+		border-radius: 12px;
+		width: 100%;
+		height: 200px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Switch from '../components/Switch.svelte';
+</script>
+
+<h3 class="title"> Enciende una bombilla a la vez </h3>
+<div class="wrapper">
+	<Switch identifier={1} />
+	<Switch identifier={2} />
+	<Switch identifier={3} />
+	<Switch identifier={4} />
+</div>
+
+<style>
+	.wrapper {
+		display: grid;
+		grid-column-gap: 2rem;
+		grid-template-columns: repeat(2, 1fr);
+		grid-row-gap: 2rem;
+	}
+	.title{
+		margin:10px 0 40px 0; 
+		text-align: center;
+	}
+	@media screen and (min-width: 768px) {
+		.wrapper {
+			display: grid;
+			grid-column-gap: 2rem;
+			grid-template-columns: repeat(4, 1fr);
+		}
+		.title{
+			font-size: 3rem;
+			margin:0px 0 40px 0; 
+		}
+	}
+</style>
